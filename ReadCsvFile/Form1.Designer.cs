889f154efd4ef433414d2c5216010f9090f86c8a
moshekagan <kaganmoshe90@gmail.com>
2015-03-14 13:05:46 +0200

    Add 2 HeadSet NeroSteer

---
+++ ReadCsvFile/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace ReadCsvFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.e1_PB = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.e1_lable = new System.Windows.Forms.Label();
+            this.e2_lable = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.e2_PB = new System.Windows.Forms.PictureBox();
+            this.e3_lable = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.e3_PB = new System.Windows.Forms.PictureBox();
+            this.h1_lable = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.h1_PB = new System.Windows.Forms.PictureBox();
+            this.h2_lable = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.h2_PB = new System.Windows.Forms.PictureBox();
+            this.theta_lable = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.theta_PB = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.e1_PB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.e2_PB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.e3_PB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.h1_PB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.h2_PB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.theta_PB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(12, 72);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(502, 854);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(285, 53);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // e1_PB
+            // 
+            this.e1_PB.BackColor = System.Drawing.Color.Magenta;
+            this.e1_PB.Location = new System.Drawing.Point(520, 830);
+            this.e1_PB.Name = "e1_PB";
+            this.e1_PB.Size = new System.Drawing.Size(107, 50);
+            this.e1_PB.TabIndex = 3;
+            this.e1_PB.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(520, 901);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "e1:";
+            // 
+            // e1_lable
+            // 
+            this.e1_lable.AutoSize = true;
+            this.e1_lable.Location = new System.Drawing.Point(564, 902);
+            this.e1_lable.Name = "e1_lable";
+            this.e1_lable.Size = new System.Drawing.Size(24, 25);
+            this.e1_lable.TabIndex = 5;
+            this.e1_lable.Text = "0";
+            // 
+            // e2_lable
+            // 
+            this.e2_lable.AutoSize = true;
+            this.e2_lable.Location = new System.Drawing.Point(700, 902);
+            this.e2_lable.Name = "e2_lable";
+            this.e2_lable.Size = new System.Drawing.Size(24, 25);
+            this.e2_lable.TabIndex = 8;
+            this.e2_lable.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(656, 901);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "e2:";
+            // 
+            // e2_PB
+            // 
+            this.e2_PB.BackColor = System.Drawing.Color.MediumVioletRed;
+            this.e2_PB.Location = new System.Drawing.Point(656, 830);
+            this.e2_PB.Name = "e2_PB";
+            this.e2_PB.Size = new System.Drawing.Size(107, 50);
+            this.e2_PB.TabIndex = 6;
+            this.e2_PB.TabStop = false;
+            // 
+            // e3_lable
+            // 
+            this.e3_lable.AutoSize = true;
+            this.e3_lable.Location = new System.Drawing.Point(842, 902);
+            this.e3_lable.Name = "e3_lable";
+            this.e3_lable.Size = new System.Drawing.Size(24, 25);
+            this.e3_lable.TabIndex = 11;
+            this.e3_lable.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(798, 901);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 25);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "e3:";
+            // 
+            // e3_PB
+            // 
+            this.e3_PB.BackColor = System.Drawing.Color.HotPink;
+            this.e3_PB.Location = new System.Drawing.Point(798, 830);
+            this.e3_PB.Name = "e3_PB";
+            this.e3_PB.Size = new System.Drawing.Size(107, 50);
+            this.e3_PB.TabIndex = 9;
+            this.e3_PB.TabStop = false;
+            // 
+            // h1_lable
+            // 
+            this.h1_lable.AutoSize = true;
+            this.h1_lable.Location = new System.Drawing.Point(978, 902);
+            this.h1_lable.Name = "h1_lable";
+            this.h1_lable.Size = new System.Drawing.Size(24, 25);
+            this.h1_lable.TabIndex = 14;
+            this.h1_lable.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(934, 901);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(42, 25);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "h1:";
+            // 
+            // h1_PB
+            // 
+            this.h1_PB.BackColor = System.Drawing.Color.Green;
+            this.h1_PB.Location = new System.Drawing.Point(934, 830);
+            this.h1_PB.Name = "h1_PB";
+            this.h1_PB.Size = new System.Drawing.Size(107, 50);
+            this.h1_PB.TabIndex = 12;
+            this.h1_PB.TabStop = false;
+            // 
+            // h2_lable
+            // 
+            this.h2_lable.AutoSize = true;
+            this.h2_lable.Location = new System.Drawing.Point(1114, 902);
+            this.h2_lable.Name = "h2_lable";
+            this.h2_lable.Size = new System.Drawing.Size(24, 25);
+            this.h2_lable.TabIndex = 17;
+            this.h2_lable.Text = "0";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(1070, 901);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(42, 25);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "h2:";
+            // 
+            // h2_PB
+            // 
+            this.h2_PB.BackColor = System.Drawing.Color.Lime;
+            this.h2_PB.Location = new System.Drawing.Point(1070, 830);
+            this.h2_PB.Name = "h2_PB";
+            this.h2_PB.Size = new System.Drawing.Size(107, 50);
+            this.h2_PB.TabIndex = 15;
+            this.h2_PB.TabStop = false;
+            // 
+            // theta_lable
+            // 
+            this.theta_lable.AutoSize = true;
+            this.theta_lable.Location = new System.Drawing.Point(1276, 902);
+            this.theta_lable.Name = "theta_lable";
+            this.theta_lable.Size = new System.Drawing.Size(24, 25);
+            this.theta_lable.TabIndex = 20;
+            this.theta_lable.Text = "0";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(1214, 901);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(66, 25);
+            this.label11.TabIndex = 19;
+            this.label11.Text = "theta:";
+            // 
+            // theta_PB
+            // 
+            this.theta_PB.BackColor = System.Drawing.Color.Yellow;
+            this.theta_PB.Location = new System.Drawing.Point(1214, 830);
+            this.theta_PB.Name = "theta_PB";
+            this.theta_PB.Size = new System.Drawing.Size(107, 50);
+            this.theta_PB.TabIndex = 18;
+            this.theta_PB.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1750, 979);
+            this.Controls.Add(this.theta_lable);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.theta_PB);
+            this.Controls.Add(this.h2_lable);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.h2_PB);
+            this.Controls.Add(this.h1_lable);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.h1_PB);
+            this.Controls.Add(this.e3_lable);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.e3_PB);
+            this.Controls.Add(this.e2_lable);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.e2_PB);
+            this.Controls.Add(this.e1_lable);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.e1_PB);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.e1_PB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.e2_PB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.e3_PB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.h1_PB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.h2_PB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.theta_PB)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox e1_PB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label e1_lable;
+        private System.Windows.Forms.Label e2_lable;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox e2_PB;
+        private System.Windows.Forms.Label e3_lable;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox e3_PB;
+        private System.Windows.Forms.Label h1_lable;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox h1_PB;
+        private System.Windows.Forms.Label h2_lable;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.PictureBox h2_PB;
+        private System.Windows.Forms.Label theta_lable;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.PictureBox theta_PB;
+    }
+}
+
